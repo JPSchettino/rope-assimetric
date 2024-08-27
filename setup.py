3cdb6040a8b4ca7470b905153d1985e37c840dd4
Joao Pedro Junqueira Schettino <schettino.joaopedro@gmail.com>
2024-08-27 00:32:15 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scaled-rope",
+    name="rope-assimetric",
     version="0.1",
     packages=find_packages(),  # Automatically finds all packages and subpackages
     install_requires=install_requires,

a1b30479cc2ba170c7dd44cc7cd81e6e6b512971
Joao Pedro Junqueira Schettino <schettino.joaopedro@gmail.com>
2024-08-27 00:30:19 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
-from setuptools import setup
-
-install_requires = []
-with open("./requirements.txt", encoding="utf-8") as requirements_file:
-    reqs = [r.strip() for r in requirements_file.readlines()]
-    reqs = [r for r in reqs if r and r[0] != "#"]
-    for r in reqs:
-        install_requires.append(r)
+from setuptools import setup, find_packages
 
 setup(
     name="scaled-rope",
     version="0.1",
-    packages=["scaled_rope"],
+    packages=find_packages(),  # Automatically finds all packages and subpackages
     install_requires=install_requires,
     url='https://github.com/JPSchettino/rope-assimetric/',
     license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-    ]
+    ],
 )
